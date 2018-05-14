5df1727b300ffdd1da801aa83f61a76fab744683
takeru911 <takeru_maehara@voyagegroup.com>
2018-05-14 09:34:46 +0900

    毎度matrix作らないように

--- liknval_recommend/src/model.py
+++ liknval_recommend/src/model.py
@@ -2,17 +2,21 @@
 import sys
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import NMF
 import scipy.sparse as sp
+import filter
+
+from sklearn.decomposition import NMF
 from scipy import io
 from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import spsolve
 
 
-def load_dense_matrix(is_init, all):
+
+def load_dense_matrix(is_init):
     print(is_init)
     R = lil_matrix((158392, 69667))
     if is_init:
+        all = pd.read_csv("../input/prepro.csv")
         for index, d in all.iterrows():
           if index % 1000 == 0:
             print("num iter: {}".format(index))
@@ -33,16 +37,31 @@ if arg[1] == "1":
 else:
     is_init = False
     
-all = pd.read_csv("../input/prepro.csv")
-R = load_dense_matrix(is_init, all)
+
+R = load_dense_matrix(is_init)
 print(R.shape)
 model = NMF(n_components = 10, init="random", random_state=0)
 W = model.fit_transform(R)
 H = model.components_
-print(W)
-#result = np.dot(W, H)
-# expect 3
-#print(result[125664, 11045])
-# expect 1
-#print(result[125664, 0])
+w = W[0:6270,:]
+h = H[:, 0:2]
+# 検証用で行列を小さく小さく
+result = np.dot(w, h)
+user_recommend_pre_list = []
+i = 1
+
+for rating_list in result.T:
+   user_recommend_pre_list.append({
+       "userId": i,
+       "events": pd.DataFrame({
+           "event_id": range(1, len(rating_list) + 1),
+           "rating": rating_list
+           })
+       })
+   i = i + 1
+log = pd.read_csv("../input/all/log.tsv", sep="\t")
+join_list = filter.user_join_list(log)
+user_not_join_list = filter.join_event_filter(1, user_recommend_pre_list[0]["events"], join_list)
+filtered_join_recommend_list = filter.join_event_filter(1, user_recommend_pre_list[0]["events"], join_list)
+
 #io.savemat("result", {"result": result})
